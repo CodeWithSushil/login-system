3543d787ae79c29f9bbbe8acaf7bead077233585
ashishkumbhar01 <ashishkumbhar01@hotmail.com>
2025-07-07 11:33:02 +0530

    update

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+(function(){
+    document.addEventListener("DOMContentLoaded", function(){
+        console.log('Welcome to PHP Login system project.');
+        
+        // start your javascript logic here
+        
+    });
+})();
