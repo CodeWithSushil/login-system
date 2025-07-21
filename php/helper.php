81c035fb1ba7d3f4776985f0f770350a45d05be7
Sushil Kumar <sushilkumbhar77@gmail.com>
2025-07-21 15:10:29 +0530

    add user ip helper class

--- php/helper.php
+++ php/helper.php
@@ -6,3 +6,20 @@ function userIpAddr(){
 
 
 }
+
+function getUserIpAddr(){
+    if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
+        // Check if from shared internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        // Check if from a proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    } else {
+        // Default, REMOTE_ADDR
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
+}
+
+$user_ip = getUserIpAddr();
+echo 'User IP Address: '.$user_ip;
